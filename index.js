c827cd9585b1c5e0da24d797d611adbe3084d2db
Mohamed-Ugas <136655335+Mohamed-Ugas@users.noreply.github.com>
2023-09-04 12:53:39 +0100

    installed inquirer and added array questions

--- index.js
+++ index.js
@@ -5,6 +5,50 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project",
+    },
+
+    {
+    type: "input",
+    name: "description",
+    message: "Describe the functionality of this project",
+    },
+
+    {
+    type: "list",
+    name: "license",
+    message: "What is the license of your project?",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "BSD2", "BSD3", "none"],
+    },
+
+    {
+        type: "input",
+        name: "install",
+        message: "What are the steps requirded to install your project? ",
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use this app?",
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?",
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+    
+
 
 ];
 
